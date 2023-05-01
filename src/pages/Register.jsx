19bcc13eb6d4950343c053df911aa3ed4b8f9f09
Lucas Braga Santos <lucasbrasantos@gmail.com>
2023-05-01 18:02:02 -0300

    update Register.jsx / add register feature

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Add from '../imgs/addAvatar.png';
+import Swal from 'sweetalert2'
+
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
 
 const Register = () => {
+    const [err, setErr] = useState(false);
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const displayName = e.target[0].value;
+        const email = e.target[1].value;
+        const password = e.target[2].value;
+        const file = e.target[3].files[0];
+        
+        createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed in
+            const user = userCredential.user;
+            console.log(user);
+            Swal.fire(
+                'Success',
+                ' ',
+                'success'
+              )
+          })
+          .catch((error) => {
+            
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(`${errorCode}\n${errorMessage}`);
+            setErr(true);
+
+            errorCode == "auth/invalid-email" ? Swal.fire({
+                title: 'Error!',
+                text: "Invalid Email",
+                icon: 'error',
+                confirmButtonText: 'Return'
+            })
+            : errorCode == "auth/weak-password" ? Swal.fire({
+                title: 'Error!',
+                text: `Weak Password, password should be at least 6 characters`,
+                icon: 'error',
+                confirmButtonText: 'Return'
+            })
+            : errorCode == "auth/email-already-in-use" ? Swal.fire({
+                title: 'Error!',
+                text: `Email Already In Use! try again with another email`,
+                icon: 'error',
+                confirmButtonText: 'Return'
+            })            
+            : console.log(`${errorCode}\n${errorMessage}`);
+
+
+
+          });
+        
+    }
+    
+
+
   return (
     <div className='formContainer'>
 
@@ -9,7 +68,7 @@ const Register = () => {
             <span className='logo'>Superchat 2.0</span>
             <hr />
             <span className='title'>Register</span>
-            <form action="">
+            <form onSubmit={handleSubmit}>
                 <input type="text" placeholder='display name'/>
                 <input type="email" placeholder='email'/>
                 <input type="password" placeholder='password'/>
@@ -18,8 +77,9 @@ const Register = () => {
                     <img src={Add} alt="" />
                     <span>Add an avatar</span>
                 </label>
-                <button>Sign up</button>
+                <button >Sign up</button>
             </form>
+                {err && <span className='error'>something went wrong! </span>}
             <p>Do you have an account? Login</p>
         </div>
 
