c8b6158898278dd516713cb2a26721d2dc3f4893
Lucas Braga Santos <lucasbrasantos@gmail.com>
2023-05-01 18:02:12 -0300

    update firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 import {initializeApp} from "firebase/app";
 import {getAuth} from "firebase/auth";
-
+import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,3 +15,4 @@ const firebaseConfig = {
   // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
+export const storage = getStorage();
