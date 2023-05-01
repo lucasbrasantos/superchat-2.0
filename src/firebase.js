95dc45e94f71e45300814782aeba6a787ecfd730
Lucas Braga Santos <lucasbrasantos@gmail.com>
2023-05-01 20:36:09 -0300

    update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
 import {initializeApp} from "firebase/app";
-import {getAuth} from "firebase/auth";
+import {getAuth, GoogleAuthProvider} from "firebase/auth";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
@@ -19,3 +19,6 @@ export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth();
 export const storage = getStorage();
+export const googleProvider = new GoogleAuthProvider();
+
+
