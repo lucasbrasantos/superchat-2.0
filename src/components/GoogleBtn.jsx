abf949ffe6fba44fd056e10cb5de74a34033e173
Lucas Braga Santos <lucasbrasantos@gmail.com>
2023-05-01 20:37:13 -0300

    create GoogleBtn.jsx component

---
+++ src/components/GoogleBtn.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const GoogleBtn = () => {
+  return (
+    <div class="google-btn">
+    <div class="google-icon-wrapper">
+        <img class="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"/>
+    </div>
+    <p class="btn-text">Sign in with google</p>
+    </div>
+  )
+}
+
+export default GoogleBtn
