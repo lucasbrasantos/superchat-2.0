b7d9df289009fb9467e664a95110bd35d2e1d653
Lucas Braga Santos <lucasbrasantos@gmail.com>
2023-05-01 20:35:55 -0300

    update App.js / add routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Home from "./pages/Home";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import './style.scss';
 
 function App() {
     return (
-        <div>
-      
-            <Register/>
-            <Login/>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/">
+                    <Route index element={<Home/>}></Route>
+                    <Route path="register" element={<Register/>}></Route>
+                    <Route path="login" element={<Login/>}></Route>
+                </Route>
+            </Routes>
 
-        </div>
+        
+        </BrowserRouter>
     );
 }
 
