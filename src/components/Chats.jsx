73d933c6960715efb9b7a1a598003bf0b79d6f80
Lucas Braga Santos <lucasbrasantos@gmail.com>
2023-05-07 20:17:23 -0300

    Chats.jsx db sync

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,24 +1,48 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { AuthContext } from '../context/AuthContext'
+import { doc, onSnapshot } from "firebase/firestore";
+import { db } from '../firebase';
+
 
 const Chats = () => {
-  return (
-    <div className='chats'>
-      <div className="userChat">
-            <img src="https://images.pexels.com/photos/936049/pexels-photo-936049.jpeg" alt="" />
-            <div className="userChatInfo">
-                <span>John</span>
-                <p>hello</p>
-            </div>
-      </div>
-      <div className="userChat">
-            <img src="https://images.pexels.com/photos/936049/pexels-photo-936049.jpeg" alt="" />
-            <div className="userChatInfo">
-                <span>John</span>
-                <p>hello</p>
-            </div>
-        </div>
-    </div>
-  )
+
+
+	const [chats, setChats] = useState([])
+	const {currentUser} = useContext(AuthContext)
+
+	useEffect(() => {
+		const getChats = () => {
+			
+			const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {			
+				setChats(doc.data());
+	
+	
+			});
+		
+			return() => {
+				unsub()
+			};			
+		}
+
+		currentUser.uid && getChats();
+
+	}, [currentUser.uid]);
+
+	console.log(Object.entries(chats));
+	return (
+	<div className='chats'>
+
+		{Object.entries(chats)?.map((chat) => (
+			<div className="userChat" key={chat[0]}>
+				<img src={chat[1].userInfo.photoURL} alt="user profile picture" />
+				<div className="userChatInfo">
+					<span>{chat[1].userInfo.displayName}</span>
+					<p>{chat[1].lastMessage?.text}</p>
+				</div>
+			</div>
+		))}
+	</div>
+	)
 }
 
 export default Chats
