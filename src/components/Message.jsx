35527fed84cf1a46b50cf4b6a5458de0999e9529
Lucas Braga Santos <lucasbrasantos@gmail.com>
2023-05-07 22:09:06 -0300

    update Message.jsx/Messages.jsx

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,18 +1,31 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
-const Message = () => {
-  return (
-    <div className='message owner'>
-      <div className="messageInfo">
-        <img src="https://images.pexels.com/photos/936049/pexels-photo-936049.jpeg" alt="" />
-       <span>just now</span>
+import { AuthContext } from '../context/AuthContext';
+import { ChatContext } from '../context/ChatContext';
+
+const Message = ({message}) => {
+
+    console.log(message);
+
+    const {currentUser} = useContext(AuthContext)
+    const {data} = useContext(ChatContext)
+    
+    return (
+      <div className='message owner'>
+        <div className="messageInfo">
+          <img
+            src=""
+            alt="" />
+        <span>just now</span>
+        </div>
+
+
+        <div className="messageContent">
+          <p>hello</p>
+          <img src="https://images.pexels.com/photos/936049/pexels-photo-936049.jpeg" alt="" />
+        </div>
       </div>
-      <div className="messageContent">
-        <p>hello</p>
-        <img src="https://images.pexels.com/photos/936049/pexels-photo-936049.jpeg" alt="" />
-      </div>
-    </div>
-  )
+    )
 }
 
 export default Message
