b0ad0b35e765dfc25c5f4735736c8b066e342654
Lucas Braga Santos <lucasbrasantos@gmail.com>
2023-05-14 21:15:26 -0300

    update Message.jsx

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,28 +1,33 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useRef } from 'react'
 
 import { AuthContext } from '../context/AuthContext';
 import { ChatContext } from '../context/ChatContext';
 
-const Message = ({message}) => {
+const Message = ({message, key}) => {
 
-    console.log(message);
+    const {currentUser} = useContext(AuthContext);
+    const {data} = useContext(ChatContext);
 
-    const {currentUser} = useContext(AuthContext)
-    const {data} = useContext(ChatContext)
+    const ref = useRef();
     
-    return (
-      <div className='message owner'>
+    useEffect(() => {
+      ref.current?.scrollIntoView({ behavior: "smooth" });
+    }, [message]);
+
+    console.log(message)
+    return (      
+      <div ref={ref} className={`message ${message.senderId === currentUser.uid && "owner"}`}>
         <div className="messageInfo">
           <img
-            src=""
+            src={message.senderId === currentUser.uid ? currentUser.photoURL : data.user.photoURL}
             alt="" />
         <span>just now</span>
         </div>
 
 
         <div className="messageContent">
-          <p>hello</p>
-          <img src="https://images.pexels.com/photos/936049/pexels-photo-936049.jpeg" alt="" />
+          <p>{message.text}</p>
+          {message.img && <img src={message.img} alt="" />}
         </div>
       </div>
     )
